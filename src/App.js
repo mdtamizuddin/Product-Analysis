6984f471ac5606a746c0a0b84e844863a9c8caf9
mdtomiz420 <tamizrabbi@gmail.com>
2022-04-04 05:53:39 +0600

    App created Adn Navbar Created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import './Navbar.css'
+const Navbar= () => {
+    const [resNav , setResnav] = useState('topnav')
+    const navControlar =()=>{
+        if (resNav === "topnav") {
+            setResnav("responsive")
+          } else {
+            setResnav("topnav")
+          }
+    }
+  return (
+    <nav>
+       <div className='nav-content'>
+       <div className='logo'>
+            <Link to='/'> <h1>MD TOMIZ</h1></Link>
+            <i onClick={navControlar} className="fa-solid fa-bars"></i>
+        </div>
+        <ul className={resNav} id='nav-manu'>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/review'>Review</NavLink></li>
+                <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                <li><NavLink to='/blogs'>Blogs</NavLink></li>
+                <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+       </div>
+    </nav>
+  )
+}
+
+export default Navbar ;
