db8246b14b1bea4587bb4665f480b60d45904850
mdtomiz420 <tamizrabbi@gmail.com>
2022-04-04 07:40:50 +0600

    useReview Hook Created

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,17 +1,24 @@
 
 
 import React from 'react'
-
+import useReview from '../../Hook/useReview'
+import Card from './Card/Card'
 import './Review.css'
 function Review() {
-   
+    const [reviews , setReview]  = useReview()
   
     return (
         <div className="reviews container">
             <h1 className='review-heading'>Customer Reviews</h1>
             <div className="review">
 
-              
+               {
+                   reviews.map(review => {
+                      return(
+                          <Card key={review} review={review}/>
+                      )
+                   })
+               }
 
                 
             </div>
