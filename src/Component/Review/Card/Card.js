d5eab28b2f992bbc9b6b04594102e3aaebf381e4
mdtomiz420 <tamizrabbi@gmail.com>
2022-04-04 07:41:36 +0600

    Review Card Created

---
+++ src/Component/Review/Card/Card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Rating } from '@mui/material';
+import './Card.css'
+
+function Card(props) {
+    
+    const setValue =()=>{
+    }
+   
+   const {name , logo,star,description}  = props.review
+  return (
+    <div className="review-card">
+    <div className="header-card">
+        <h5 className="logo">{logo}</h5>
+        <div className="name">
+            <h3>{name}</h3>
+        </div>
+       
+    </div>
+    <Rating
+            name="simple-controlled"
+            value={star}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+        />
+
+    <p className="description">{description.slice(0 , 270)}</p>
+</div>
+  )
+}
+
+export default Card
